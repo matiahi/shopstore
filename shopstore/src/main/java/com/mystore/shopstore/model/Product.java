677cb9337563d94ecf31a5b3f7c30384cd35bc1b
matiahi <matiahi@yahoo.com>
2025-05-05 17:19:25 -0600

    Add Product entity and repository

---
+++ shopstore/src/main/java/com/mystore/shopstore/model/Product.java
@@ -0,0 +1,69 @@
+package com.mystore.shopstore.model;
+
+import java.math.BigDecimal;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private String imageUrl;
+
+    public Product() {
+
+    }
+
+    public Product(String name, String description, BigDecimal price, String imageUrl) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imageUrl = imageUrl;
+
+    }
+
+    // Getter and Setter
+    public long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
