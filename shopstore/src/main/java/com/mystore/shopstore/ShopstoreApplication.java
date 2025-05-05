c32dce40e3e5368a5cafc032341e11a5c045b9f7
matiahi <matiahi@yahoo.com>
2025-05-05 14:27:02 -0600

    Initial commit: Spring Boot setup

---
+++ shopstore/src/main/java/com/mystore/shopstore/ShopstoreApplication.java
@@ -0,0 +1,13 @@
+package com.mystore.shopstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopstoreApplication.class, args);
+	}
+
+}
