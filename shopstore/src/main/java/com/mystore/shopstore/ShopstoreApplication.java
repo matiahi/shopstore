b3b1f68f071afca9d5da2ba99743ed6fdfe7d57a
matiahi <matiahi@yahoo.com>
2025-05-10 20:11:34 -0600

    Add custom alert to admin UI

--- shopstore/src/main/java/com/mystore/shopstore/ShopstoreApplication.java
+++ shopstore/src/main/java/com/mystore/shopstore/ShopstoreApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import io.micrometer.common.lang.NonNull;
+import org.springframework.lang.NonNull;
 
 @SpringBootApplication
 public class ShopstoreApplication {
